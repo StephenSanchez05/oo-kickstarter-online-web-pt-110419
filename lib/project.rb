348207c26472bbd25afd9638dbbbeb1d615a51d2
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-21 05:37:32 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,9 @@
 class Project
   
-  attr_accessor :name 
-  
-  attr_reader :backers
+  attr_reader :backers, :title
   
   def initialize(name)
-    @name = name
+    @title = name
     @backers = []
   end
 end 
